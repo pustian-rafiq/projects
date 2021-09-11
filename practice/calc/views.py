dc922f628e7c5cf0c7665ea2c52bc4da6693a347
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-09-11 15:06:57 +0600

    add two numbers

--- practice/calc/views.py
+++ practice/calc/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return render(request,'home.html',{'name':'Rabiul'})
 
+def home(request):
+    return render(request, 'home.html', {'name': 'Rafiqul'})
+
+
+def add(request):
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    result = val1 + val2
+    return render(request, 'result.html', {'result': result})
